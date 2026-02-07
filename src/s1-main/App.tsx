2c49a25e778e2b11cda4a210518681f02f293b23
Leopoldiev <leoflamefire777@gmail.com>
2026-02-07 16:35:44 +0300

    initial commit

---
+++ src/s1-main/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from './App.module.css'
+import Junior from "../s2-homeworks/hw05/pages/Junior";
+
+function App() {
+    return (
+        <div className={s.App}>
+            {/*<HW1/>*/}
+            {/*раскомментировать по ходу выполнения*/}
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
+
+            {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
+            {/*<HW5/>*/}
+            {/*<HW6/>*/}
+            <Junior/>
+        </div>
+    )
+}
+
+export default App
